b9ba07ea5d389d99e96c90d8248dbbd286135987
Yuzhong Zhang <141388234+BetterAndBetterII@users.noreply.github.com>
2025-07-05 23:27:59 +0800

    app init

--- excalidraw/excalidraw-app/index.tsx
+++ excalidraw/excalidraw-app/index.tsx
@@ -4,12 +4,24 @@ import ExcalidrawApp from "./App";
 import { registerSW } from "virtual:pwa-register";
 
 import "../excalidraw-app/sentry";
+import { PostHogProvider } from "posthog-js/react";
+
 window.__EXCALIDRAW_SHA__ = import.meta.env.VITE_APP_GIT_SHA;
 const rootElement = document.getElementById("root")!;
 const root = createRoot(rootElement);
 registerSW();
 root.render(
   <StrictMode>
-    <ExcalidrawApp />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        defaults: "2025-05-24",
+        capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+        debug: import.meta.env.MODE === "development",
+      }}
+    >
+      <ExcalidrawApp />
+    </PostHogProvider>
   </StrictMode>,
 );
