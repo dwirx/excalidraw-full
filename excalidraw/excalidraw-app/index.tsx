41b7d7f06af7492cadeca2fcf32c07b2fff3e2c6
Yuzhong Zhang <141388234+BetterAndBetterII@users.noreply.github.com>
2025-07-06 12:58:20 +0800

    移除posthog

--- excalidraw/excalidraw-app/index.tsx
+++ excalidraw/excalidraw-app/index.tsx
@@ -4,7 +4,6 @@ import ExcalidrawApp from "./App";
 import { registerSW } from "virtual:pwa-register";
 
 import "../excalidraw-app/sentry";
-import { PostHogProvider } from "posthog-js/react";
 
 window.__EXCALIDRAW_SHA__ = import.meta.env.VITE_APP_GIT_SHA;
 const rootElement = document.getElementById("root")!;
@@ -12,16 +11,6 @@ const root = createRoot(rootElement);
 registerSW();
 root.render(
   <StrictMode>
-    <PostHogProvider
-      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
-      options={{
-        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-        defaults: "2025-05-24",
-        capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
-        debug: import.meta.env.MODE === "development",
-      }}
-    >
-      <ExcalidrawApp />
-    </PostHogProvider>
+    <ExcalidrawApp />
   </StrictMode>,
 );
